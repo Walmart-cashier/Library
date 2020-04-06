c3181d752d4e475172317295c70a57275e2a1ff5
mohdsauood <35588119+mohdsauood@users.noreply.github.com>
2020-04-06 21:56:55 +0400

    fixed layout and wired submit button

---
+++ script.js
@@ -0,0 +1,73 @@
+window.addEventListener('load',()=>{
+    
+    
+ 
+const addBookButton=document.querySelector('.addButton');
+const addForm=document.querySelector('.addingBook div');
+const submitForm=document.querySelector('.submitButton');  
+
+
+    
+    
+    
+    
+//class toggler for adding book
+addBookButton.addEventListener('click',()=>{
+    if (addForm.classList.contains('hidebook')) {
+  addForm.classList.remove('hidebook');
+  addForm.classList.add('addBook','formbackground');
+} else {
+  addForm.classList.remove('addBook','formbackground');
+  addForm.classList.add('hidebook');
+}
+    
+});
+    
+//class toggler when hit submit button
+
+
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+});
